497ed207da647002bf390ba8983f63084aec1d26
m2gi-davidge <geoffrey.david@etu.univ-grenoble-alpes.fr>
2023-10-10 09:28:45 +0200

    fix: JvnServerImpl add debug

--- SOURCES/src/jvn/JvnServerImpl.java
+++ SOURCES/src/jvn/JvnServerImpl.java
@@ -2,15 +2,13 @@
  * JAVANAISE Implementation
  * JvnServerImpl class
  * Implementation of a Jvn server
- * Contact: 
- *
+ * Contact:
  * Authors: 
  */
 
 package jvn;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -26,6 +24,7 @@ public class JvnServerImpl
   /**
 	 *
 	 */
+	@Serial
 	private static final long serialVersionUID = 1L;
 	// A JVN server is managed as a singleton 
 	private static JvnServerImpl js = null;
@@ -34,7 +33,6 @@ public class JvnServerImpl
 
   /**
   * Default constructor
-  * @throws JvnException
   **/
 	private JvnServerImpl() throws MalformedURLException, NotBoundException, RemoteException {
 		super();
@@ -48,14 +46,13 @@ public class JvnServerImpl
   /**
     * Static method allowing an application to get a reference to 
     * a JVN server instance
-    * @throws JvnException
     **/
 	public static JvnServerImpl jvnGetServer() throws JvnException {
 		if (js == null){
 			try {
 				js = new JvnServerImpl();
 			} catch (Exception e) {
-				throw new JvnException("Cannot get the server instance: "+ e.getMessage());
+				throw new JvnException("JvnGetServer: Cannot get the server instance: "+ e.getMessage());
 			}
 		}
 		return js;
@@ -63,7 +60,6 @@ public class JvnServerImpl
 	
 	/**
 	* The JVN service is not used anymore
-	* @throws JvnException
 	**/
 	public  void jvnTerminate()
 	throws jvn.JvnException {
@@ -77,7 +73,6 @@ public class JvnServerImpl
 	/**
 	* creation of a JVN object
 	* @param o : the JVN object state
-	* @throws JvnException
 	**/
 	public  JvnObject jvnCreateObject(Serializable o)
 	throws jvn.JvnException { 
@@ -92,8 +87,7 @@ public class JvnServerImpl
 	/**
 	*  Associate a symbolic name with a JVN object
 	* @param jon : the JVN object name
-	* @param jo : the JVN object 
-	* @throws JvnException
+	* @param jo : the JVN object
 	**/
 	public  void jvnRegisterObject(String jon, JvnObject jo)
 	throws jvn.JvnException {
@@ -110,8 +104,7 @@ public class JvnServerImpl
 	/**
 	* Provide the reference of a JVN object beeing given its symbolic name
 	* @param jon : the JVN object name
-	* @return the JVN object 
-	* @throws JvnException
+	* @return the JVN object
 	**/
 	public  JvnObject jvnLookupObject(String jon)
 	throws jvn.JvnException {
@@ -132,13 +125,13 @@ public class JvnServerImpl
 	 * Called by JvnObject.
 	* @param joi : the JVN object identification
 	* @return the current JVN object state
-	* @throws  JvnException
 	**/
    public Serializable jvnLockRead(int joi)
 	 throws JvnException {
 	   try {
 		   return jvnRemoteCoord.jvnLockRead(joi, this);
 	   } catch (RemoteException e){
+		   e.printStackTrace();
 		   throw new JvnException("jvnLockRead: "+e.getMessage());
 	   }
 	}
@@ -148,13 +141,13 @@ public class JvnServerImpl
 	 * Called by JvnObject.
 	* @param joi : the JVN object identification
 	* @return the current JVN object state
-	* @throws  JvnException
 	**/
    public Serializable jvnLockWrite(int joi)
 	 throws JvnException {
 	   try {
 		   return jvnRemoteCoord.jvnLockWrite(joi, this);
 	   } catch (RemoteException e){
+		   e.printStackTrace();
 		   throw new JvnException("jvnLockWrite: "+e.getMessage());
 	   }
 	}	
@@ -164,8 +157,6 @@ public class JvnServerImpl
 	* Invalidate the Read lock of the JVN object identified by id 
 	* called by the JvnCoord
 	* @param joi : the JVN object id
-	* @return void
-	* @throws java.rmi.RemoteException,JvnException
 	**/
   public void jvnInvalidateReader(int joi)
 	throws java.rmi.RemoteException,jvn.JvnException {
@@ -178,7 +169,6 @@ public class JvnServerImpl
 	* Invalidate the Write lock of the JVN object identified by id 
 	* @param joi : the JVN object id
 	* @return the current JVN object state
-	* @throws java.rmi.RemoteException,JvnException
 	**/
   public Serializable jvnInvalidateWriter(int joi)
 	throws java.rmi.RemoteException,jvn.JvnException {
@@ -191,7 +181,6 @@ public class JvnServerImpl
 	* Reduce the Write lock of the JVN object identified by id 
 	* @param joi : the JVN object id
 	* @return the current JVN object state
-	* @throws java.rmi.RemoteException,JvnException
 	**/
    public Serializable jvnInvalidateWriterForReader(int joi)
 	 throws java.rmi.RemoteException,jvn.JvnException {
