d08d0b31b4d0508f5ba5f679490c3b98f86a946d
m2gi-davidge <geoffrey.david@etu.univ-grenoble-alpes.fr>
2023-10-16 10:24:24 +0200

    feat: ADd ISentence to permit proxies casting

--- SOURCES/src/irc/Irc2.java
+++ SOURCES/src/irc/Irc2.java
@@ -16,7 +16,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.Serializable;
 
 
 public class Irc2 {
