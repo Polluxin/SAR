ed743e0887c538753260429d9d7ad99477cbc3b5
Polluxin <geoffrey.david@orange.fr>
2023-10-22 16:27:25 +0200

    test: IrcManipulator replace AdvancedIrc.java

--- SOURCES/src/irc/IrcManipulator.java
+++ SOURCES/src/irc/IrcManipulator.java
@@ -1,4 +1,4 @@
-package advancedIrc;
+package irc;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,20 +6,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class AdvancedIrc {
+public class IrcManipulator {
 
     JFrame frame;
+    TextField objName;
 
     public static void main(String[] argv) {
-        new AdvancedIrc();
+        new IrcManipulator();
     }
 
-    AdvancedIrc(){
+    IrcManipulator(){
         frame = new JFrame();
+        frame.setLayout(new GridLayout());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         Button create = new Button("Create an IRC");
         create.addActionListener(new createListener());
 
+        objName = new TextField();
+
+
+        frame.add(objName);
         frame.add(create);
         frame.setSize(545,201);
         frame.setVisible(true);
@@ -34,7 +41,7 @@ public class AdvancedIrc {
         public void actionPerformed(ActionEvent e) {
             Runtime runtime = Runtime.getRuntime();
             try {
-                runtime.exec("java -jar .\\SOURCES\\SAR.jar");
+                runtime.exec("java -jar .\\SOURCES\\Irc2.jar "+objName.getText());
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
