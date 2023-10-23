65003cc92c53a41fb0e32e019549e970c534a6ec
m2gi-davidge <geoffrey.david@etu.univ-grenoble-alpes.fr>
2023-10-23 10:47:18 +0200

    feat: burst test added

--- BurstExec.sh
+++ BurstExec.sh
@@ -1,10 +1,7 @@
-CLASSPATH="../Irc2Burst.jar:../irc"
-MAIN_CLASS="irc.Irc2ForBurst"
-
 # Execution
-java -cp "$CLASSPATH" "$MAIN_CLASS" "jaja" "1" &
-java -cp "$CLASSPATH" "$MAIN_CLASS" "dede" "2" &
-java -cp "$CLASSPATH" "$MAIN_CLASS" "sisi" "3" &
-java -cp "$CLASSPATH" "$MAIN_CLASS" "toto" "4" &
+java -jar BurstIrc2.jar "jaja" "1" &
+java -jar BurstIrc2.jar "dede" "2" &
+java -jar BurstIrc2.jar "sisi" "3" &
+java -jar BurstIrc2.jar "toto" "4" &
 
 wait
